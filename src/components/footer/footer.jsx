c6f5ed0c6c6353361bdb13c1d3d26d79379b1e76
AGpeter2018 <adenijipeter2018@gmail.com>
2025-10-05 16:48:29 +0100

    added footer and its styling

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./footer.style.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright &copy; PortalChain - ALL Right reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
