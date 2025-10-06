d25a89aa91785a1c61de8140c1471d42d4608df0
AGpeter2018 <adenijipeter2018@gmail.com>
2025-10-06 14:25:46 +0100

    created user profile  authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "./firebase-utils";
 
 import Navbar from "./components/navbar/navbar";
 import Home from "./pages/home/home";
@@ -6,8 +9,30 @@ import Coin from "./pages/coin/coin";
 import Footer from "./components/footer/footer";
 import SignUp from "./components/sign-up-/sign-up";
 import SignIn from "./components/sign-in/sign-in";
+import { createUserProfile } from "./firebase-utils";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("");
+  const subscription = auth.onAuthStateChanged(async (user) => {
+    if (user) {
+      const userRef = await createUserProfile(user);
+      userRef.onSnapshot((snapshot) => {
+        setCurrentUser(
+          currentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
+          })
+        );
+      });
+    }
+  });
+  useEffect(() => {
+    // if (subscription) {
+    //   return subscription();
+    // }
+    subscription();
+  }, [subscription]);
+
   return (
     <div className="App">
       <Navbar />
